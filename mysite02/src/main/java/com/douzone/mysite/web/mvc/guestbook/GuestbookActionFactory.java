6a3f3fcdb30040d56e4457312eb3ee39e32d2d21
Ahhhhhhyeong <dkdud081@gmail.com>
2022-05-25 15:42:23 +0900

    test

--- mysite02/src/main/java/com/douzone/mysite/web/mvc/guestbook/GuestbookActionFactory.java
+++ mysite02/src/main/java/com/douzone/mysite/web/mvc/guestbook/GuestbookActionFactory.java
@@ -1,21 +1,21 @@
-package com.douzone.mysite.web.mvc.user;
+package com.douzone.mysite.web.mvc.guestbook;
 
 import com.douzone.mysite.web.mvc.main.DefaultAction;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.mvc.ActionFactory;
 
-public class UserActionFactory extends ActionFactory {
-
+public class GuestbookActionFactory extends ActionFactory {
+	
 	@Override
 	public Action getAction(String actionName) {
 		Action action = null;
 		
 		if("joinform".equals(actionName)) {
-			action = new JoinFormAction();
+			//action = new JoinFormAction();
 		} else if("join".equals(actionName)) {
-			action = new JoinAction();
+			//action = new JoinAction();
 		} else if("joinsuccess".equals(actionName)) {
-			action = new JoinSuccess();
+			//action = new JoinSuccess();
 		} else {
 			action = new DefaultAction();
 		}

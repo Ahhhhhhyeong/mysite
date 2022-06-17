b1c9a0654f3940772a11cc71dda796adf515fd5e
Ahhhhhhyeong <dkdud081@gmail.com>
2022-06-17 11:41:42 +0900

    mysite04 #fin?

--- mysite04/src/main/java/com/douzone/mysite/config/web/WebConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/web/WebConfig.java
@@ -9,12 +9,15 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.douzone.config.web.FileUploadConfig;
+import com.douzone.config.web.MessageConfig;
 import com.douzone.config.web.MvcConfig;
+import com.douzone.config.web.SecurityConfig;
 import com.douzone.mysite.interceptor.SiteInterceptor;
 
 @Configuration
 @EnableAspectJAutoProxy
-@Import({MvcConfig.class})
+@Import({MvcConfig.class, SecurityConfig.class,  MessageConfig.class, FileUploadConfig.class})
 @ComponentScan({"com.douzone.mysite.controller", "com.douzone.mysite.exception"})
 public class WebConfig implements WebMvcConfigurer{
 
